afe0bdd32f90ff7fd9271bb755e12e5049b240c3
hugoiman <hugogiman17@gmail.com>
2020-07-02 10:16:22 +0700

    Tugas 1 Javascript

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+var a = 20;
+var b = "Dua Puluh";
+var c = true;
+var d = null;
+var e;
+var f = [1, 2, 3, 4, 5];
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
